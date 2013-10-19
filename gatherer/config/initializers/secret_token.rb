a99fdf8c4eb7df57d6761cf271c426b910f7f663
Noel Rappin <noel@tablexi.com>
2013-10-19 10:33:53 -0500

    rvm files

---
+++ gatherer/config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Gatherer::Application.config.secret_key_base = '9cbb1cdd81bd1e79999d8f91ec57b0ced0e59ec961f9af56e77a5e514acf7cc9f646e83aedd5293f44685c47e717eade8677fca153d9c65ffb441d4fdff33052'
