fdbf54e6a285778f26218c887bd0c7cbb2a69c37
Noel Rappin <noel@tablexi.com>
2013-10-29 21:47:51 -0500

    refactoring:

--- gatherer/test/models/project_with_data_test.rb
+++ gatherer/test/models/project_with_data_test.rb
@@ -2,14 +2,22 @@ require 'test_helper'
 
 class ProjectWithDataTest < ActiveSupport::TestCase
 
-  test "a project can tell how much is left" do
-    project = Project.new
+  setup :create_project_with_data
+
+  def create_project_with_data
+    @project = Project.new
     done = Task.new(size: 2, completed: true)
     small_not_done = Task.new(size: 1)
     large_not_done = Task.new(size: 4)
-    project.tasks = [done, small_not_done, large_not_done]
-    assert_equal(7, project.total_size)
-    assert_equal(5, project.remaining_size)
+    @project.tasks = [done, small_not_done, large_not_done]
+  end
+
+  test "a project can tell its total size" do
+    assert_equal(7, @project.total_size)
+  end
+
+  test "a project can tell how much is left" do
+    assert_equal(5, @project.remaining_size)
   end
 
 end
