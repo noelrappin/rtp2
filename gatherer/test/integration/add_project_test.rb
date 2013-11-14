16ef3c278f39b88cb3fcaa38911ebf0f80834e1e
Noel Rappin <noel@tablexi.com>
2013-11-13 19:58:59 -0600

    now with form

--- gatherer/test/integration/add_project_test.rb
+++ gatherer/test/integration/add_project_test.rb
@@ -3,9 +3,9 @@ require "test_helper"
 class AddProjectTest < Capybara::Rails::TestCase
   test "a user can add a a project and give it tasks" do
     visit new_project_path
-    fill_in "Title", with: "Project Runway"         # <label id="when_start" />
+    fill_in "Name", with: "Project Runway"         # <label id="when_start" />
     fill_in "Tasks", with: "Task 1:3\nTask2:5"
-    click_on("Submit")
+    click_on("Create Project")
     visit projects_path                             # <label id="when_end" />
     assert_content("Project Runway")                # <label id="then_start" />
     assert_content("8")
