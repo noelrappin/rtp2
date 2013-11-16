193d9b348f4bed855cab8ff337266dbbf8e40b95
Noel Rappin <noel@tablexi.com>
2013-11-16 11:44:41 -0600

    first action

---
+++ gatherer/test/actions/creates_project_test.rb
@@ -0,0 +1,11 @@
+require 'test_helper'
+
+class CreatesProjectTest < ActiveSupport::TestCase
+
+  test "creates a project given a name" do
+    creator = CreatesProject.new(name: "Project Runway")
+    creator.build
+    assert_equal "Project Runway", creator.project.name
+  end
+
+end
