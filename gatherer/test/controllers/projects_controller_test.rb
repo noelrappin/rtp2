ecd413f0fe12e9266a42b4b47ffd1e43bc7b39d1
Noel Rappin <noel@tablexi.com>
2013-11-22 19:27:20 -0600

    code labels

--- gatherer/test/controllers/projects_controller_test.rb
+++ gatherer/test/controllers/projects_controller_test.rb
@@ -3,9 +3,9 @@ require 'test_helper'
 class ProjectsControllerTest < ActionController::TestCase
 
   test "the project method creates a project" do
-    post :create, project: {name: "Runway", tasks: "start something:2"}
-    assert_redirected_to projects_path
-    assert_equal "Runway", assigns(:action).project.name
+    post :create, project: {name: "Runway", tasks: "start something:2"} # <label id="call_post" />
+    assert_redirected_to projects_path # <label id="assert_redirect" />
+    assert_equal "Runway", assigns(:action).project.name # <label id="assert_assigns" />
   end
 
 end
