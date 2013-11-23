8e7598292c89bed590619654e30045d49968d52d
Noel Rappin <noel@tablexi.com>
2013-11-22 22:39:07 -0600

    more labels

--- gatherer/test/controllers/projects_controller_test.rb
+++ gatherer/test/controllers/projects_controller_test.rb
@@ -3,16 +3,16 @@ require 'test_helper'
 class ProjectsControllerTest < ActionController::TestCase
 
   test "the project method creates a project" do
-    post :create, project: {name: "Runway", tasks: "start something:2"} # <label id="call_post" />
-    assert_redirected_to projects_path # <label id="assert_redirect" />
-    assert_equal "Runway", assigns[:action].project.name # <label id="assert_assigns" />
+    post :create, project: {name: "Runway", tasks: "start something:2"}
+    assert_redirected_to projects_path
+    assert_equal "Runway", assigns[:action].project.name
   end
 
   ##START: failure
   test "on failure we go back to the form" do
-    post :create, project: {name: "", tasks: ""}
-    assert_template :new
-    refute_nil assigns(:project)
+    post :create, project: {name: "", tasks: ""} # <label id="blank_form" />
+    assert_template :new # <label id="assert_template" />
+    refute_nil assigns(:project) # <label id="refute_nil" />
   end
   ##END: failure
 end
