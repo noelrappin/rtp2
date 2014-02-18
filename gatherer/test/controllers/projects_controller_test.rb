3ce3e3e388a097a60b4feabfc4e9261c1338bd31
Noel Rappin <noel@tablexi.com>
2014-02-17 19:55:08 -0600

    controller test label

--- gatherer/test/controllers/projects_controller_test.rb
+++ gatherer/test/controllers/projects_controller_test.rb
@@ -2,11 +2,13 @@ require 'test_helper'
 
 class ProjectsControllerTest < ActionController::TestCase
 
+  ##START:state_test
   test "the project method creates a project" do
     post :create, project: {name: "Runway", tasks: "start something:2"}
     assert_redirected_to projects_path
     assert_equal "Runway", assigns[:action].project.name
   end
+  ##END:state_test
 
   ##START: failure
   test "on failure we go back to the form" do
