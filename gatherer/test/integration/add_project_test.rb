5f5fb3aad34d48c0f894ea29437a274ca5c11bb6
Noel Rappin <noel@tablexi.com>
2014-03-08 10:00:14 -0600

    moving a label

--- gatherer/test/integration/add_project_test.rb
+++ gatherer/test/integration/add_project_test.rb
@@ -5,9 +5,9 @@ class AddProjectTest < Capybara::Rails::TestCase
     visit new_project_path                          # <label id="when_start" />
     fill_in "Name", with: "Project Runway"
     fill_in "Tasks", with: "Task 1:3\nTask2:5"
-    click_on("Create Project")
-    visit projects_path                             # <label id="when_end" />
-    assert_content("Project Runway")                # <label id="then_start" />
+    click_on("Create Project")                      # <label id="when_end" />
+    visit projects_path                             # <label id="then_start" />
+    assert_content("Project Runway")
     assert_content("8")
   end
 
