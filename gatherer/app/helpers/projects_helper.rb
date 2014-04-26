326cbe08df07954767fb40fb0a89af9418041f54
Noel Rappin <noel@tablexi.com>
2014-04-26 13:54:11 -0500

    passing helper test

--- gatherer/app/helpers/projects_helper.rb
+++ gatherer/app/helpers/projects_helper.rb
@@ -1,2 +1,6 @@
 module ProjectsHelper
+
+  def name_with_status(project)
+    content_tag(:span, project.name, class: 'on_schedule') # <label id="code.content_tag" />
+  end
 end
--- gatherer/test/helpers/projects_helper_test.rb
+++ gatherer/test/helpers/projects_helper_test.rb
@@ -3,10 +3,10 @@ require 'test_helper'
 class ProjectsHelperTest < ActionView::TestCase
 
   test "project name with status info" do
-    project = Project.new(name: "Project Runway")
-    project.stubs(:on_schedule?).returns(true)
-    actual = name_with_status(project)
-    expected = "<span class='on_schedule'>Project Runway</span>"
-    assert_dom_equal expected, actual
+    project = Project.new(name: "Project Runway") # <label id="code.create" />
+    project.stubs(:on_schedule?).returns(true) # <label id="code.mock_project" />
+    actual = name_with_status(project) # <label id="code.actual" />
+    expected = "<span class='on_schedule'>Project Runway</span>" # <label id="code.expected" />
+    assert_dom_equal expected, actual # <label id="code.comparison" />
   end
 end
