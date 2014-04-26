f072b9403e20bfa8375f3052c2493bce6118bab1
Noel Rappin <noel@tablexi.com>
2014-04-26 13:04:56 -0500

    failing helper test

--- gatherer/test/helpers/projects_helper_test.rb
+++ gatherer/test/helpers/projects_helper_test.rb
@@ -1,4 +1,12 @@
 require 'test_helper'
 
 class ProjectsHelperTest < ActionView::TestCase
+
+  test "project name with status info" do
+    project = Project.new(name: "Project Runway")
+    project.stubs(:on_schedule?).returns(true)
+    actual = name_with_status(project)
+    expected = "<span class='on_schedule'>Project Runway</span>"
+    assert_dom_equal expected, actual
+  end
 end
