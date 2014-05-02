142003077e7bf8ac74d519514d231f14a1a6f729
Noel Rappin <noel@tablexi.com>
2014-05-01 22:04:57 -0500

    labels

--- gatherer/app/presenters/project_presenter.rb
+++ gatherer/app/presenters/project_presenter.rb
@@ -1,14 +1,14 @@
 class ProjectPresenter < SimpleDelegator
 
-  def self.from_project_list(*projects)
+  def self.from_project_list(*projects) # <label id="code.presenter_factory" />
     projects.flatten.map { |project| ProjectPresenter.new(project) }
   end
 
-  def initialize(project)
+  def initialize(project) # <label id="code.presenter_constructor" />
     super
   end
 
-  def name_with_status
+  def name_with_status # <label id="code.presenter_method" />
     dom_class = on_schedule? ? 'on_schedule' : 'behind_schedule'
     "<span class='#{dom_class}'>#{name}</span>"
   end
