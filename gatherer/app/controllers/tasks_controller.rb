6fe615ac2eb87efaf4e1622e6b7a99d685a3c564
Noel Rappin <noel@tablexi.com>
2014-05-03 10:35:30 -0500

    now with mail

--- gatherer/app/controllers/tasks_controller.rb
+++ gatherer/app/controllers/tasks_controller.rb
@@ -2,7 +2,10 @@ class TasksController < ApplicationController
 
   def update
     @task = Task.find(params[:id])
-    if @task.update_attributes(params[:task].permit(:size))
+    completed = params[:task].delete(:completed)
+    params[:task][:completed_at] = Time.current if completed
+    if @task.update_attributes(params[:task].permit(:size, :completed_at))
+      TaskMailer.task_completed_email(@task).deliver if completed
       redirect_to @task, notice: "'project was successfully updated.'"
     else
       render action: 'edit'
