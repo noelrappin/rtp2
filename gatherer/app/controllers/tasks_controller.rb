c1a36400e80daaafdd7d77b63625b50c22d5de75
Noel Rappin <noel@tablexi.com>
2014-05-03 09:28:40 -0500

    no mail test passes

--- gatherer/app/controllers/tasks_controller.rb
+++ gatherer/app/controllers/tasks_controller.rb
@@ -1,2 +1,15 @@
 class TasksController < ApplicationController
+
+  def update
+    @task = Task.find(params[:id])
+    if @task.update_attributes(params[:task].permit(:size))
+      redirect_to @task, notice: "'project was successfully updated.'"
+    else
+      render action: 'edit'
+    end
+  end
+
+  def show
+    @task = Task.find(params[:id])
+  end
 end
--- gatherer/test/controllers/tasks_controller_test.rb
+++ gatherer/test/controllers/tasks_controller_test.rb
@@ -1,7 +1,16 @@
 require 'test_helper'
 
 class TasksControllerTest < ActionController::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+
+  setup do
+    ActionMailer::Base.deliveries.clear # <label id="code.clear_mailers" />
+  end
+
+  test "on update with no completion, no email is sent" do
+    task = Task.create!(title: "Write section on testing mailers",
+        size: 2)
+    patch :update, id: task.id, task: {size: 3}
+    assert_equal 0, ActionMailer::Base.deliveries.size # <label id="code.no_emails" />
+  end
+
 end
