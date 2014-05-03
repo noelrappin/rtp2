b8557fe6a978606283bdc274cf122ee59bd852c4
Noel Rappin <noel@tablexi.com>
2014-05-02 21:33:10 -0500

    now with updates and minitest

--- gatherer/test/presenters/project_presenter_test.rb
+++ gatherer/test/presenters/project_presenter_test.rb
@@ -1,19 +1,19 @@
 require 'test_helper'
 
-class ProjectPresenterTest < ActiveSupport::TestCase
+class ProjectPresenterTest < Minitest::Test
 
-  setup do
+  def setup
     @project = Project.new(name: "Project Runway")
     @presenter = ProjectPresenter.new(@project)
   end
 
-  test "project name with status info" do
+  def test_project_name_with_status_info
     @project.stubs(:on_schedule?).returns(true)
     expected = "<span class='on_schedule'>Project Runway</span>"
     assert_equal(expected, @presenter.name_with_status)
   end
 
-  test "project name with status info behind sechdule" do
+  def test_project_name_with_status_info_behind_sechdule
     @project.stubs(:on_schedule?).returns(false)
     expected = "<span class='behind_schedule'>Project Runway</span>"
     assert_equal(expected, @presenter.name_with_status)
