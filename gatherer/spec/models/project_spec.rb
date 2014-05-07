925394c9b33fe5bf0a3b0023ff50be024aac7ab4
Noel Rappin <noel@tablexi.com>
2014-05-06 19:35:49 -0500

    initial rspec

---
+++ gatherer/spec/models/project_spec.rb
@@ -0,0 +1,24 @@
+require 'spec_helper'
+
+describe Project do
+
+  describe "with a new project" do
+    before(:each) do
+      @project = Project.new
+    end
+
+    it "knows that a project with no tasks is done" do
+      expect(@project.done?).to be_truthy
+    end
+
+    it "knows that a project with an incomplete task is done" do
+      task = Task.new
+      @project.tasks << task
+      expect(@project.done?).to be_falsy
+    end
+
+
+  end
+
+
+end
