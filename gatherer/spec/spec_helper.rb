4b12f4adeb2615548b8122bd3ac5c07633f6bd1e
Noel Rappin <noel@tablexi.com>
2014-05-07 21:58:48 -0500

    helper change

--- gatherer/spec/spec_helper.rb
+++ gatherer/spec/spec_helper.rb
@@ -25,6 +25,11 @@ RSpec.configure do |config|
   # config.mock_with :flexmock
   # config.mock_with :rr
 
+  config.mock_with :rspec do |mocks|
+    mocks.verify_doubled_constant_names = true
+    mocks.verify_partial_doubles = true
+  end
+
   # Remove this line if you're not using ActiveRecord or ActiveRecord fixtures
   config.fixture_path = "#{::Rails.root}/spec/fixtures"
 
