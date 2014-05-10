497b3ab98fd3bd0e45d9beaccdd1179ee78cc8be
Noel Rappin <noel@tablexi.com>
2014-05-10 14:17:44 -0500

    view tests

---
+++ gatherer/spec/controllers/projects_controller_spec.rb
@@ -0,0 +1,30 @@
+require 'spec_helper'
+
+describe ProjectsController do
+
+  describe "#create" do
+    it "creates a project" do
+      post :create, project: {name: "Runway", tasks: "start something:2"}
+      expect(response).to redirect_to(projects_path)
+      expect(assigns(:action).project.name).to eq("Runway")
+    end
+
+    it "creates a project with mocks" do
+      fake_project = double(create: true)
+      allow(CreatesProject).to receive(:new)
+          .with(name: "Runway", task_string: "start something:2")
+          .and_return(fake_project)
+      post :create, project: {name: "Runway", tasks: "start something:2"}
+      expect(CreatesProject).to have_received(:new)
+      expect(response).to redirect_to(projects_path)
+      expect(assigns(:action)).not_to be_nil
+    end
+
+    it "fails gracefully" do
+      allow_any_instance_of(Project).to receive(:save).and_return(false)
+      expect { post :create, :project => {:name => 'Project Runway'} }
+        .not_to change { Project.count }
+    end
+  end
+
+end
---
+++ gatherer/spec/views/projects/index.html.erb_spec.rb
@@ -0,0 +1,32 @@
+# describe "#index" do
+  #   let(:completed_task) { Task.create!(completed_at: 1.day.ago, size: 1) }
+  #   let(:on_schedule) { Project.create!(due_date: 1.year.from_now,
+  #       name: "On Schedule", tasks: [completed_task] }
+  #   let(:incomplete_task) { Task.create!(size: 1) }
+  #   let(:behind_schedule) { Project.create!(due_date: 1.day.from_now,
+  #       name: "Behind Schedule", tasks: [incomplete_task] }
+  #   it "does something" do
+
+  #   end
+  # end
+
+require 'spec_helper'
+
+describe "projects/index" do
+  let(:completed_task) { Task.create!(completed_at: 1.day.ago, size: 1) }
+  let(:on_schedule) { Project.create!(due_date: 1.year.from_now,
+      name: "On Schedule", tasks: [completed_task]) }
+  let(:incomplete_task) { Task.create!(size: 1) }
+  let(:behind_schedule) { Project.create!(due_date: 1.day.from_now,
+      name: "Behind Schedule", tasks: [incomplete_task]) }
+
+  it "renders the index page with correct dom elements" do
+    @projects = ProjectPresenter.from_project_list(
+      [on_schedule, behind_schedule])
+    render
+    expect(rendered).to have_selector(
+        "#project_#{on_schedule.id} .on_schedule")
+    expect(rendered).to have_selector(
+        "#project_#{behind_schedule.id} .behind_schedule")
+  end
+end
