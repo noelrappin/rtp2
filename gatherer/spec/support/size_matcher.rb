29f2ae21b2d767fdac035d4806a9176fe9424a77
Noel Rappin <noel@tablexi.com>
2014-05-11 12:44:31 -0500

    with add ons

--- gatherer/spec/support/size_matcher.rb
+++ gatherer/spec/support/size_matcher.rb
@@ -2,4 +2,16 @@ RSpec::Matchers.define :have_size do |expected|
   match do |actual|
     actual.total_size == expected
   end
+
+  description do
+    "have tasks totaling #{expected} points"
+  end
+
+  failure_message do |actual|
+    "expected project #{project.name} to have size #{actual}"
+  end
+
+  failure_message_when_negated do |actual|
+    "expected project #{project.name} not to have size #{acutal}"
+  end
 end
