6eb45e12f016c9b6b7c9bc1f345524b3b76776b9
Noel Rappin <noel@tablexi.com>
2014-05-20 22:06:35 -0500

    adding task steps

---
+++ gatherer/features/step_definitions/add_task_steps.rb
@@ -0,0 +1,27 @@
+Given(/^a project$/) do
+  pending # express the regexp above with the code you wish you had
+end
+
+When(/^I visit the project page$/) do
+  pending # express the regexp above with the code you wish you had
+end
+
+When(/^I complete the new task form$/) do
+  pending # express the regexp above with the code you wish you had
+end
+
+Then(/^I am back on the project page$/) do
+  pending # express the regexp above with the code you wish you had
+end
+
+Then(/^I see the new task is last in the list$/) do
+  pending # express the regexp above with the code you wish you had
+end
+
+When(/^I click to move the new task up$/) do
+  pending # express the regexp above with the code you wish you had
+end
+
+Then(/^the new task is in the middle of the list$/) do
+  pending # express the regexp above with the code you wish you had
+end
