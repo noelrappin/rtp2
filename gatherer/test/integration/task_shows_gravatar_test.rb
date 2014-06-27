47fcb8805c77909e2f853fba85bd15d1c2e4afb1
Noel Rappin <noel@tablexi.com>
2014-06-26 23:53:18 -0500

    fix

--- gatherer/test/integration/task_shows_gravatar_test.rb
+++ gatherer/test/integration/task_shows_gravatar_test.rb
@@ -14,10 +14,10 @@ class TaskShowsGravatar < Capybara::Rails::TestCase
   test "i see a gravatar" do
     VCR.use_cassette("loading gravatar") do
       visit project_path(projects(:bluebook))
+      url = "http://www.gravatar.com/avatar/6b767d8a4c9910e007c122d81eb4de73"
       within("#task_1") do
         assert_selector(".completed_by", text: users(:user).email)
-        assert_selector(
-          "img[src='http://www.gravatar.com/avatar/6b767d8a4c9910e007c122d81eb4de7']")
+        assert_selector("img[src='#{url}']")
       end
     end
   end
