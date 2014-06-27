9ed420b2c53f45568e5bca933a966e05a6909205
Noel Rappin <noel@tablexi.com>
2014-06-26 23:24:40 -0500

    stating

---
+++ gatherer/test/integration/task_shows_gravatar_test.rb
@@ -0,0 +1,22 @@
+require "test_helper"
+
+class TaskShowsGravatar < Capybara::Rails::TestCase
+
+  include Warden::Test::Helpers
+
+  setup do
+    projects(:bluebook).roles.create(user: users(:user))
+    users(:user).update_attributes(email: "noelrappin@gmail.com")
+    tasks(:one).update_attributes(user_id: users(:user).id)
+    login_as users(:user)
+  end
+
+  test "i see a gravatar" do
+    visit project_path(projects(:bluebook))
+    within("#task_1") do
+      assert_selector(".completed_by", text: users(:user).email)
+      assert_selector(
+        "img[src='http://www.gravatar.com/avatar/6b767d8a4c9910e007c122d81eb4de7']")
+    end
+  end
+end
