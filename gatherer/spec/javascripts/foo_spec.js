b9e6d4e0b410405df14be162055ee2a40a525778
Noel Rappin <noel@tablexi.com>
2014-07-04 08:31:56 -0500

    removing foo

--- gatherer/spec/javascripts/foo_spec.js
+++
@@ -1,9 +0,0 @@
-describe('Foo', function() {
-  it("does something", function() {
-    expect(1 + 1).toBe(2);
-  });
-
-  it("does something else", function() {
-    expect(1 + 2).toBe(2);
-  });
-});
