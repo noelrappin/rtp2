8ece515db0395bc49a958ed935ad7f1e05f30d97
Noel Rappin <noel@tablexi.com>
2014-07-04 18:19:16 -0500

    integration test

---
+++ gatherer/spec/javascripts/taks_move_spec.js
@@ -0,0 +1,16 @@
+describe("with a list of tasks", function() {
+
+  beforeEach(function() {
+    table = affix("table");
+    table.affix("tr.task#task_1 a.up");
+    table.affix("tr.task#task_2 a.up+a.down");
+    table.affix("tr.task#task_3 a.up");
+  });
+
+  it("correctly processes a click", function() {
+    $("#task_2 .up").click();
+    expect($.map($("tr"), function(item) { return $(item).attr("id") }))
+        .toEqual(["task_2", "task_1", "task_3"]);
+  });
+
+});
