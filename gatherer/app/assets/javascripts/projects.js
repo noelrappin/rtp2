e9f82fb0646a91c133794b843cdcc3dbf1289184
Noel Rappin <noel@tablexi.com>
2014-07-06 13:11:12 -0500

    preventing default

--- gatherer/app/assets/javascripts/projects.js
+++ gatherer/app/assets/javascripts/projects.js
@@ -28,7 +28,8 @@ var Project = {
 }
 
 $(function() {
-  $(document).on("click", ".up", function() {
+  $(document).on("click", ".up", function(event) {
+    event.preventDefault();
     Project.upClickOn($(this));
   });
 })
