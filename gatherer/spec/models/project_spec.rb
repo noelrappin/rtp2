373a229cf46c78d5fc5ddfc6e77e0436547b863d
Noel Rappin <noel@tablexi.com>
2014-08-08 11:52:01 -0500

    new files

---
+++ gatherer/spec/models/project_spec.rb
@@ -0,0 +1,8 @@
+require 'rails_helper' # <label id="code.require_rails" />
+
+RSpec.describe Project do # <label id="code.describe" />
+  it "considers a project with no test to be done" do # <label id="code.id" />
+    project = Project.new
+    expect(project.done?).to be_truthy # <label id="code.expect" />
+  end
+end
