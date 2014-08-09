e3f95095360cac2bf7f212b3533eff8b14f73d62
Noel Rappin <noel@tablexi.com>
2014-08-08 23:52:46 -0500

    project spec

--- gatherer/spec/models/project_spec.rb
+++ gatherer/spec/models/project_spec.rb
@@ -25,7 +25,7 @@ RSpec.describe Project do
     it "properly estimates a blank project" do
       expect(project.completed_velocity).to eq(0)
       expect(project.current_rate).to eq(0)
-      expect(project.projected_days_remaining).to eq(NaN)
+      expect(project.projected_days_remaining.nan?).to be_truthy
       expect(project).not_to be_on_schedule
     end
     ##END: init_project
