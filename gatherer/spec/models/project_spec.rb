adc714c9a8da58f26fa483beb20288a697a0b4e3
Noel Rappin <noel@tablexi.com>
2014-08-08 23:49:40 -0500

    tests pass

--- gatherer/spec/models/project_spec.rb
+++ gatherer/spec/models/project_spec.rb
@@ -20,6 +20,15 @@ RSpec.describe Project do
       task.mark_completed
       expect(project).to be_done
     end
+
+    ##START: init_project
+    it "properly estimates a blank project" do
+      expect(project.completed_velocity).to eq(0)
+      expect(project.current_rate).to eq(0)
+      expect(project.projected_days_remaining).to eq(NaN)
+      expect(project).not_to be_on_schedule
+    end
+    ##END: init_project
   end
 
   describe "estimates" do
