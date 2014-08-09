58c15a844dcf301667451a99264cc46a4cba9419
Noel Rappin <noel@tablexi.com>
2014-08-08 23:46:55 -0500

    tests pass

--- gatherer/app/models/project.rb
+++ gatherer/app/models/project.rb
@@ -1,6 +1,6 @@
 class Project
 
-  attr_accessor :tasks
+  attr_accessor :tasks, :due_date
 
   def initialize
     @tasks = []
@@ -21,4 +21,23 @@ class Project
   def remaining_size
     incomplete_tasks.sum(&:size)
   end
+
+  ##START:new_math
+  def completed_velocity
+    tasks.sum(&:points_toward_velocity)
+  end
+
+  def current_rate
+    completed_velocity * 1.0 / 21
+  end
+
+  def projected_days_remaining
+    remaining_size / current_rate
+  end
+
+  def on_schedule?
+    (Date.today + projected_days_remaining) <= due_date
+  end
+
+  ##END:new_math
 end
