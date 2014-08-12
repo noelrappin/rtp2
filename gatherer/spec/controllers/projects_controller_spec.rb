0e616d8580cceb40661861531ade8294888e4abd
Noel Rappin <noel@tablexi.com>
2014-08-12 00:05:51 -0500

    label change

--- gatherer/spec/controllers/projects_controller_spec.rb
+++ gatherer/spec/controllers/projects_controller_spec.rb
@@ -6,7 +6,7 @@ RSpec.describe ProjectsController, :type => :controller do
     it "creates a project" do
       post :create, project: {name: "Runway", tasks: "Start somethig:2"} # <label id="code.call_post" />
       expect(response).to redirect_to(projects_path) # <label id="code.redirect" />
-      expect(assigns(:action).project.name).to eq("Runway") # <label id="code.assigns" />
+      expect(assigns(:action).project.name).to eq("Runway") # <label id="code.spec_assigns" />
     end
   end
 
