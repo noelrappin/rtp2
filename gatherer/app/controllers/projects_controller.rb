b540afd7c808510dc2a94225fafd82589dadda60
Noel Rappin <noel@tablexi.com>
2014-08-11 21:39:39 -0500

    controller test

--- gatherer/app/controllers/projects_controller.rb
+++ gatherer/app/controllers/projects_controller.rb
@@ -3,4 +3,14 @@ class ProjectsController < ApplicationController
   def new
     @project = Project.new
   end
+
+  ##START:create
+  def create
+    @action = CreatesProject.new(
+        name: params[:project][:name],
+        task_string: params[:project][:tasks])
+    @action.create
+    redirect_to projects_path
+  end
+  ##END:create
 end
--- gatherer/spec/controllers/projects_controller_spec.rb
+++ gatherer/spec/controllers/projects_controller_spec.rb
@@ -2,4 +2,12 @@ require 'rails_helper'
 
 RSpec.describe ProjectsController, :type => :controller do
 
+  describe "POST create" do
+    it "creates a project" do
+      post :create, project: {name: "Runway", tasks: "Start somethig:2"} # <label id="code.call_post" />
+      expect(response).to redirect_to(projects_path) # <label id="code.redirect" />
+      expect(assigns(:action).project.name).to eq("Runway") # <label id="code.assigns" />
+    end
+  end
+
 end
