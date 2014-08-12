5710ef05b83ea2a6ba6627d79d0d05ace36b2a3b
Noel Rappin <noel@tablexi.com>
2014-08-11 23:57:19 -0500

    fi

--- gatherer/spec/features/add_project_spec.rb
+++ gatherer/spec/features/add_project_spec.rb
@@ -3,11 +3,11 @@ require "rails_helper"
 describe "adding projects" do
 
   it "allows a user to create a project with tasks" do
-    visit new_project_path # <label id="code.when_start" />
+    visit new_project_path
     fill_in "Name", with: "Project Runway"
     fill_in "Tasks", with: "Task 1:3\nTask 2:5"
-    click_on("Create Project") # <label id="code.when_end" />
-    visit projects_path # <label id="code.then_start" />
+    click_on("Create Project")
+    visit projects_path
     expect(page).to have_content("Project Runway")
     expect(page).to have_content("8")
   end
