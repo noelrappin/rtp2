731068fd66470e1f455a1316efe717d0095274c3
Noel Rappin <noel@tablexi.com>
2014-08-20 06:52:47 -0500

    rspec

--- gatherer/spec/controllers/tasks_controller_spec.rb
+++ gatherer/spec/controllers/tasks_controller_spec.rb
@@ -2,4 +2,17 @@ require 'rails_helper'
 
 RSpec.describe TasksController, :type => :controller do
 
+  before(:example) do
+    ActionMailer::Base.deliveries.clear # <label id="code.clear_mailers" />
+  end
+
+  describe "PATCH update" do
+    let(:task) { Task.create!(title: "Write section on testing mailers", size: 2) }
+
+    it "does not send an email if a task is not completed" do
+      patch :update, id: task.id, task: {size: 3}
+      expect(ActionMailer::Base.deliveries.size).to eq(0) # <label id="code.no_emails" />
+    end
+  end
 end
+
