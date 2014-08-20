c5e5627b57e76a5edfd954a714543eb4f1f9ebcc
Noel Rappin <noel@tablexi.com>
2014-08-19 23:23:44 -0500

    rspec

--- gatherer/spec/helpers/projects_helper_spec.rb
+++ gatherer/spec/helpers/projects_helper_spec.rb
@@ -6,8 +6,15 @@ RSpec.describe ProjectsHelper, :type => :helper do
   it "augments with status info" do
     allow(project).to receive(:on_schedule?).and_return(true) # <label id="code.mock_project" />
     actual = helper.name_with_status(project) # <label id="code.actual" />
-    expected = "<span class='on_schedule'>Project Runway</span>" # <label id="code.expected" />
     expect(actual).to have_selector("span.on_schedule", text: "Project Runway")
   end
+
+  ##START:second_test
+  it "augments project name with status info when behind schedule" do
+    allow(project).to receive(:on_schedule?).and_return(false)
+    actual = helper.name_with_status(project)
+    expect(actual).to have_selector("span.behind_schedule", text: "Project Runway")
+  end
+  ##END:second_test
 
 end
