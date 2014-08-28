1d0b14fb66c56d435b8295a85b389945edc77554
Noel Rappin <noel@tablexi.com>
2014-08-27 22:22:25 -0500

    fix

--- gatherer/test/integration/user_and_role_test.rb
+++ gatherer/test/integration/user_and_role_test.rb
@@ -6,7 +6,7 @@ class UserAndRoleTest < Capybara::Rails::TestCase
     visit new_user_session_path
     fill_in("user_email", :with => user.email)
     fill_in("user_password", :with => user.password)
-    click_button("Sign in")
+    click_button("Log in")
   end
 
   setup do
