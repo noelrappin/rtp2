75db75f28ad3dd14108ab5660cb3783b1132dce1
Noel Rappin <noel@tablexi.com>
2014-08-27 23:25:04 -0500

    rspec

--- gatherer/spec/models/user_spec.rb
+++ gatherer/spec/models/user_spec.rb
@@ -1,4 +1,4 @@
-require 'spec_helper'
+require 'rails_helper'
 
 describe User do
 
@@ -18,7 +18,7 @@ describe User do
   ##START:public
   describe "public roles" do
     let(:user) { User.new }
-    let(:project) { Project.new }
+    let(:project) { Project.create!(name: "Project Runway") }
 
     it "allows an admin to view a project" do
       user.admin = true
@@ -26,9 +26,43 @@ describe User do
     end
 
     it "allows a public project to be seen by anyone" do
-      project.public = true
+      project.update_attributes(public: true)
       expect(user.can_view?(project)).to be_truthy
     end
   end
   ##END:public
+
+  ##START:index
+  describe "visible project listing" do
+    let(:user) { User.create!(email: "user@example.com", password: "password") }
+    let!(:project_1) { Project.create!(name: "Project 1") }
+    let!(:project_2) { Project.create!(name: "Project 2") }
+
+    before(:all) do
+      Project.delete_all
+    end
+
+    it "allows a user to see their projects" do
+      user.projects << project_1
+      expect(user.visible_projects).to eq([project_1])
+    end
+
+    it "allows an admin to see all projects" do
+      user.admin = true
+      expect(user.visible_projects).to eq([project_1, project_2])
+    end
+
+    it "allows a user to see public projects" do
+      user.projects << project_1
+      project_2.update_attributes(public: true)
+      expect(user.visible_projects).to eq([project_1, project_2])
+    end
+
+    it "has no dupes in project list" do
+      user.projects << project_1
+      project_1.update_attributes(public: true)
+      expect(user.visible_projects).to eq([project_1])
+    end
+  end
+  ##END:index
 end
