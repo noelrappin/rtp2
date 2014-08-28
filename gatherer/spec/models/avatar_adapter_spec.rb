d95cbb04b1d73ade1e5b5b1ac36ca8f5f76891d2
Noel Rappin <noel@tablexi.com>
2014-08-28 16:22:54 -0500

    rspec

---
+++ gatherer/spec/models/avatar_adapter_spec.rb
@@ -0,0 +1,12 @@
+require 'rails_helper'
+
+describe AvatarAdapter do
+
+  it "accurately receives image url", :vcr do
+    user = double(twitter_handle: "noelrap")
+    adapter = AvatarAdapter.new(user)
+    url = "http://pbs.twimg.com/profile_images/40008602/head_shot_bigger.jpg"
+    expect(adapter.image_url).to eq(urlg)
+  end
+
+end
