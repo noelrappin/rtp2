9309cbd3b346e4b5b2fd79879e71a2d16339b5b1
Noel Rappin <noel@tablexi.com>
2014-08-29 16:10:26 -0500

    lable

--- gatherer/app/controllers/projects_controller.rb
+++ gatherer/app/controllers/projects_controller.rb
@@ -1,8 +1,10 @@
 class ProjectsController < ApplicationController
 
+  ##START:index
   def index
     @projects = ProjectPresenter.from_project_list(current_user.visible_projects)
   end
+  ##END:index
 
   def new
     @project = Project.new
