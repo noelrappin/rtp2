c1cd8eb237c4a2dd6d049bdf3879d460f96f2d4c
Noel Rappin <noel@tablexi.com>
2014-09-02 10:38:12 -0500

    label

--- gatherer/spec/active_record_spec_helper.rb
+++ gatherer/spec/active_record_spec_helper.rb
@@ -7,7 +7,7 @@ ActiveRecord::Base.establish_connection(connection_info)
 
 RSpec.configure do |config|
   config.around do |example|
-    ActiveRecord::Base.transaction do # <label id="rollback" />
+    ActiveRecord::Base.transaction do # <label id="code.rollback" />
       example.run
       raise ActiveRecord::Rollback
     end
