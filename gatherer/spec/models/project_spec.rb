87e7885228db12548ff68890054dec8740408c42
Noel Rappin <noel@tablexi.com>
2014-11-13 23:18:49 -0600

    typo:

--- gatherer/spec/models/project_spec.rb
+++ gatherer/spec/models/project_spec.rb
@@ -1,7 +1,7 @@
 require 'rails_helper' # <label id="code.require_rails" />
 
 RSpec.describe Project do # <label id="code.describe" />
-  it "considers a project with no test to be done" do # <label id="code.id" />
+  it "considers a project with no tasks to be done" do # <label id="code.id" />
     project = Project.new
     expect(project.done?).to be_truthy # <label id="code.expect" />
   end
