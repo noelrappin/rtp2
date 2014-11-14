e36095e1b290908f9c8a7ee7f174e7a50f481da1
Noel Rappin <noel@tablexi.com>
2014-11-14 16:31:48 -0600

    typo

--- gatherer/test/controllers/projects_controller_test.rb
+++ gatherer/test/controllers/projects_controller_test.rb
@@ -28,7 +28,7 @@ class ProjectsControllerTest < ActionController::TestCase # <label id="code.inhe
     fake_project = mock(create: true)
     CreatesProject.expects(:new)
         .with(name: "Runway", task_string: "start something:2")
-        .returns(fake_action)
+        .returns(fake_project)
     post :create, project: {name: "Runway", tasks: "start something:2"}
     assert_redirected_to projects_path
     refute_nil assigns[:action]
